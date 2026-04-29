ae0b30a6a7eaa14323334f8fa5fc6fd3bad0c3ea
psenk <psenk168@gmail.com>
2026-04-29 14:11:26 -0500

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
-import { renderHtml } from "./renderHtml";
+import { Router } from 'itty-router';
 
-export default {
-	async fetch(request, env) {
-		const stmt = env.DB.prepare("SELECT * FROM comments LIMIT 3");
-		const { results } = await stmt.all();
-
-		return new Response(renderHtml(JSON.stringify(results, null, 2)), {
-			headers: {
-				"content-type": "text/html",
-			},
-		});
-	},
-} satisfies ExportedHandler<Env>;
+// create router instance
+const router = Router();
